1f7e9aeaf92c0123a4ef44a815062bc468da3129
archivescode <archivescode@gmail.com>
2017-02-04 14:02:06 +0700

    update header-sidebar-content

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+<div class="pull-right hidden-xs">
+  <b>Version</b> 1.0.1
+</div>
+<strong>Copyright &copy; 2014-2017 <a href="http://museumdesain.com">POC TEAM</a>.</strong> All rights
+reserved.
+</footer>
